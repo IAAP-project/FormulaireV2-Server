033eddc3e5adeeb6ae2aeae3b8a6b9c5b6fe0bbe
XyT <bobba-land@hotmail.fr>
2021-06-08 13:37:36 +0200

    edits

---
+++ BoxChoice.py
@@ -0,0 +1,4 @@
+class BoxChoice:
+    def __init__(self, name):
+        self.name = name
+        self.selected = False
---
+++ RadioInput.py
@@ -0,0 +1,8 @@
+import Input
+
+
+class RadioInput(Input):
+    def __init__(self, name):
+        super.__init__(self, name)
+        self.valeurs = ""
+
